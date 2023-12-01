7aca9f3e46192b08b22f14a4cfa37151ef6d9fcb
Victor Hernandez <victormanu968@gmail.com>
2023-11-30 20:32:54 -0500

    agregando multipliacion paralela v1

--- app/src/main/scala/taller4/Taller4.scala
+++ app/src/main/scala/taller4/Taller4.scala
@@ -9,6 +9,8 @@ import org.scalameter.measure
 import org.scalameter.withWarmer
 import org.scalameter.Warmer
 import scala.util.Random
+import implAlgoritmos._
+import common._
 
 object Taller4{
 
@@ -39,7 +41,7 @@ object Taller4{
     //calculo usando prodPunto y la transpuesta
     val l=m1.length
     Vector.tabulate(l, l)((i, j) => prodPunto(m1(i), transpuesta(m2)(j)))
-}
+  } 
   def sumMatriz(m1: Matriz, m2: Matriz): Matriz = {
     val l=m1.length
     Vector.tabulate(l, l)((i, j) => m1(i)(j)+m2(i)(j))
@@ -57,20 +59,50 @@ object Taller4{
  
   def main(args: Array[String]): Unit = {
 
-    println(matrizAlAzar(2,3))
-    println(matrizAlAzar(2,5))
-    println(prodPunto(vectorAlAzar(3,3),vectorAlAzar(3,5)))
-    println(transpuesta(matrizAlAzar(8,2)))
-    println(multMatriz(matrizAlAzar(3,3),matrizAlAzar(3,5)))
-    println(sumMatriz(matrizAlAzar(3,3),matrizAlAzar(3,5)))
-    println(restaMatriz(matrizAlAzar(3,3),matrizAlAzar(3,5)))
-    println(subMatriz(matrizAlAzar(4,10),0,1,2))
+    // println(matrizAlAzar(2,3))
+    // println(matrizAlAzar(2,5))
+    // println(prodPunto(vectorAlAzar(3,3),vectorAlAzar(3,5)))
+    // println(transpuesta(matrizAlAzar(8,2)))
+    // println(multMatriz(matrizAlAzar(3,3),matrizAlAzar(3,5)))
+    // println(sumMatriz(matrizAlAzar(3,3),matrizAlAzar(3,5)))
+    // println(restaMatriz(matrizAlAzar(3,3),matrizAlAzar(3,5)))
+    // println(subMatriz(matrizAlAzar(4,10),0,1,2))
 
-    // println(saludo())
-    // println(
-    //   withWarmer(new Warmer.Default) measure {
-    //     (1 to 100000000).toArray
-    //   } // ) 
+
+    // val m1 = Vector(Vector(3, 2, 0), Vector(6, 7, 8), Vector(1, 9, 2))
+    // val m2 = Vector(Vector(3, 2, 9), Vector(5, 2, 4), Vector(5, 6, 5))
+
+    // val resultado = multMatrizPar(m1, m2)
+    // println(resultado)
+    println("Multiplicación de matrices secuencial") 
+    val promedioSeq = (1 to 100).map(_ => 0.0).toArray
+    for (i <- 0 until 100) {
+
+      val m1 = matrizAlAzar(16, 2)
+      val m2 = matrizAlAzar(16, 2)
+      val time = withWarmer(new Warmer.Default) measure {
+        multMatriz(m1, m2)
+      }
+      promedioSeq(i) = time.value
+      println(" Repetición " + i + " tiempo: " + time)
+
+    }
+
+    val promedioPar = (1 to 100).map(_ => 0.0).toArray
+    for (i <- 0 until 100) {
+      val m1 = matrizAlAzar(16, 2)
+      val m2 = matrizAlAzar(16, 2)
+      val time = withWarmer(new Warmer.Default) measure {
+        multMatrizPar(m1, m2)
+      }
+      promedioPar(i) = time.value
+      println(" Repetición " + i + " tiempo: " + time)
+    }
+
+
+    println("El promedio secuencial es: " + promedioSeq.sum / 100 + " ms")
+    println("El promedio paralelo es: " + promedioPar.sum / 100 + " ms")
+    println("La aceleracion es de: " + (promedioSeq.sum) / (promedioPar.sum))
   }
 
  }
--- app/src/main/scala/taller4/implAlgoritmos.scala
+++ app/src/main/scala/taller4/implAlgoritmos.scala
@@ -3,8 +3,9 @@ import org.scalameter.measure
 import org.scalameter.withWarmer
 import org.scalameter.Warmer
 import scala.util.Random
+import common._
 
-class implAlgoritmos {     
+object implAlgoritmos {     
     type Matriz = Vector [ Vector [ Int ] ]
 
   def matrizAlAzar(Long: Int, vals: Int): Matriz = {
@@ -29,11 +30,38 @@ class implAlgoritmos {
   }
 
 
-def multMatriz(m1: Matriz, m2: Matriz): Matriz = {
-    //calculo usando prodPunto y la transpuesta
-    val l=m1.length
-    Vector.tabulate(l, l)((i, j) => prodPunto(m1(i), transpuesta(m2)(j)))
-}
+  def multMatriz(m1: Matriz, m2: Matriz): Matriz = {
+      //calculo usando prodPunto y la transpuesta
+      val l=m1.length
+      Vector.tabulate(l, l)((i, j) => prodPunto(m1(i), transpuesta(m2)(j)))
+  }
+
+
+  def multMatrizPar(m1: Matriz, m2: Matriz): Matriz = {
+    val filasM1 = m1.length
+    val columnasM2 = m2(0).length
+
+    def calcularElemento(fila: Vector[Int], columnaTranspuesta: Vector[Int]): Int = {
+      prodPunto(fila, columnaTranspuesta)
+    }
+
+    def crearTarea(i: Int, j: Int): Int = {
+      val tarea = task {
+        val fila = m1(i)
+        val columnaTranspuesta = transpuesta(m2)(j)
+        calcularElemento(fila, columnaTranspuesta)
+      }
+      tarea.join
+    }
+
+    val tareas = for {
+      i <- 0 until filasM1
+      j <- 0 until columnasM2
+    } yield crearTarea(i, j)
+
+    val resultados = tareas.map(_.toInt).toVector
+    resultados.grouped(m1.length).toVector
+   }
 
   def sumMatriz(m1: Matriz, m2: Matriz): Matriz = {
     val l=m1.length
@@ -44,7 +72,8 @@ def multMatriz(m1: Matriz, m2: Matriz): Matriz = {
     val l=m1.length
     Vector.tabulate(l, l)((i, j) => m1(i)(j)-m2(i)(j))
   }
-   def subMatriz (m:Matriz , i: Int , j : Int , l : Int ) : Matriz ={
+
+  def subMatriz (m:Matriz , i: Int , j : Int , l : Int ) : Matriz ={
     val sub = Vector.tabulate(l/l, l/l)((is, js) => m(i)(j))
     sub
   }
