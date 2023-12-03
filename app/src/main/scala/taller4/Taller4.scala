6ebe3feae192ce21715912c69b56584d677ba6f6
Victor Hernandez <victormanu968@gmail.com>
2023-12-03 17:46:38 -0500

    Diferentes pruebas implementadas

--- app/src/main/scala/taller4/Taller4.scala
+++ app/src/main/scala/taller4/Taller4.scala
@@ -23,7 +23,7 @@ object Taller4{
   // PRUEBAS DE RENDIMIENTOS
   def main(args: Array[String]): Unit = {
 
-    val tamanomatriz = 1
+    val tamanomatriz = 8
 
     val promedioParv2 = (1 to 100).map(_ => 0.0).toArray
     for (i <- 0 until 100) {
@@ -58,10 +58,79 @@ object Taller4{
       println(" Repetición " + i + " tiempo: " + time)
     }
 
+
+    val promedioSeqRec = (1 to 100).map(_ => 0.0).toArray
+    for (i <- 0 until 100) {
+      val m1 = matrizAlAzar(tamanomatriz, 2)
+      val m2 = matrizAlAzar(tamanomatriz, 2)
+      val time = withWarmer(new Warmer.Default) measure {
+        multMatrizRec(m1, m2)
+      }
+      promedioSeqRec(i) = time.value
+      println(" Repetición " + i + " tiempo: " + time)
+    }
+
+    // val promedioRecPar = (1 to 100).map(_ => 0.0).toArray
+    // for (i <- 0 until 100) {
+    //   val m1 = matrizAlAzar(tamanomatriz, 2)
+    //   val m2 = matrizAlAzar(tamanomatriz, 2)
+    //   val time = withWarmer(new Warmer.Default) measure {
+    //     multMatrizRecPar(m1, m2)
+    //   }
+    //   promedioRecPar(i) = time.value
+    //   println(" Repetición " + i + " tiempo: " + time)
+    // }
+
+
+      val promedioStrassen = (1 to 100).map(_ => 0.0).toArray
+      for (i <- 0 until 100) {
+        val m1 = matrizAlAzar(tamanomatriz, 2)
+        val m2 = matrizAlAzar(tamanomatriz, 2)
+        val time = withWarmer(new Warmer.Default) measure {
+          strassen(m1, m2)
+        }
+        promedioStrassen(i) = time.value
+        println(" Repetición " + i + " tiempo: " + time)
+      }
+
+      val promedioStrassenPar = (1 to 100).map(_ => 0.0).toArray
+      for (i <- 0 until 100) {
+        val m1 = matrizAlAzar(tamanomatriz, 2)
+        val m2 = matrizAlAzar(tamanomatriz, 2)
+        val time = withWarmer(new Warmer.Default) measure {
+          strassenParallel(m1, m2)
+        }
+        promedioStrassenPar(i) = time.value
+        println(" Repetición " + i + " tiempo: " + time)
+      }
+
     println("El promedio secuencial es: " + promedioSeq.sum / 100 + " ms")
     println("El promedio paralelo 1 es: " + promedioParv1.sum / 100 + " ms")
     println("El promedio paralelo 2 es: " + promedioParv2.sum / 100 + " ms")
+    println("El promedio recursivo es: " + promedioSeqRec.sum / 100 + " ms")
+    // println("El promedio recursivo paralelo es: " + promedioRecPar.sum / 100 + " ms")
+    println("El promedio Strassen es: " + promedioStrassen.sum / 100 + " ms")
+    println("El promedio Strassen paralelo es: " + promedioStrassenPar.sum / 100 + " ms")
+
+
+
+
     //println("La aceleracion es de: " + (promedioParv1.sum) / (promedioParv2.sum))
+
+
+
+
+    // val m1 = Vector(Vector(3, 2, 0), Vector(6, 7, 8), Vector(1, 9, 2))
+
+    // val m2 = Vector(Vector(3, 2, 9), Vector(5, 2, 4), Vector(5, 6, 5))
+    // println("Resultado secuencial:")
+    // println(multMatriz(m1, m2))
+
+    // println("resultado recursivo:")
+    // println(multMatrizRec(m1, m2))
+
+    // println("resultado recursivo paralelo:")
+    // println(multMatrizRecPar(m1, m2))
   }
 
  }
--- app/src/main/scala/taller4/implAlgoritmos.scala
+++ app/src/main/scala/taller4/implAlgoritmos.scala
@@ -5,6 +5,8 @@ import org.scalameter.Warmer
 import scala.util.Random
 import common._
 
+//----
+ 
 object implAlgoritmos {     
     type Matriz = Vector [ Vector [ Int ] ]
 
@@ -113,7 +115,6 @@ object implAlgoritmos {
     
   }
     
-
   def sumMatriz(m1: Matriz, m2: Matriz): Matriz = {
     val l=m1.length
     Vector.tabulate(l, l)((i, j) => m1(i)(j)+m2(i)(j))
@@ -128,4 +129,194 @@ object implAlgoritmos {
     val sub = Vector.tabulate(l/l, l/l)((is, js) => m(i)(j))
     sub
   }
+
+
+  def subMatriz2(m: Matriz, filaFrom: Int, filaTo: Int, columnaFrom: Int,columnaTo: Int): Matriz = {
+    val sub = m.slice(filaFrom, filaTo).map(_.slice(columnaFrom, columnaTo))
+    sub
+  }
+  
+  
+
+  def multMatrizRec (m1:Matriz , m2: Matriz ) : Matriz = {
+
+    def auxSumaVectorMatriz(matriz: Matriz, posicion: Int, acumuladorSuma : Vector[Vector[Int]]): Vector[Int]={
+      if (posicion == matriz.length) {acumuladorSuma(0)}
+      else{
+        val suma = sumMatriz(subMatriz(matriz,(posicion),0,matriz.length),acumuladorSuma.appended(Vector(0)))
+        auxSumaVectorMatriz(matriz,posicion+1,suma)
+      }
+    }
+
+    def modificarVector(vector:Vector[Int], posicion:Int,tamanio:Int, acumulador:Vector[Vector[Int]]) :Matriz={
+      if (posicion == tamanio) {acumulador}
+      else{
+        val nuevoVector = acumulador:+Vector(vector(posicion))
+        modificarVector(vector,posicion+1,tamanio,nuevoVector)
+      }
+    }
+
+    def auxMultMatrizRec (m1:Matriz , m2: Matriz,auxMatriz: Matriz, tamanio : Int,posicionQuieta:Int,posicionCambiante:Int,auxVector: Vector[Int] ) : Matriz ={
+     if (auxMatriz.length == m1.length ) {auxMatriz}
+     else{
+      val vectorSuma = Vector.tabulate(tamanio)((i) => prodPunto(subMatriz(m1,posicionQuieta,i,m1.length)(0), transpuesta(subMatriz(m2,i,posicionCambiante,m2.length))(0)))
+      val nuevoVector= modificarVector(vectorSuma,0,vectorSuma.length,Vector())
+      val aux2 = auxVector ++ auxSumaVectorMatriz(nuevoVector,0,Vector())
+      
+      if(aux2.length == tamanio){
+        val nuevaMatriz = auxMatriz:+aux2
+        auxMultMatrizRec(m1,m2,nuevaMatriz,tamanio,posicionQuieta+1,0,Vector())
+
+      }else{
+        if (posicionCambiante == tamanio-1) {auxMultMatrizRec(m1,m2,auxMatriz,tamanio,posicionQuieta+1,0,aux2)}
+      else{ auxMultMatrizRec(m1,m2,auxMatriz,tamanio,posicionQuieta,posicionCambiante+1,aux2)}
+      
+      } 
+    }
+    }
+    val matriz: Matriz= Vector()
+
+    auxMultMatrizRec(m1,m2,matriz,m1.length,0,0,Vector())
+  }
+
+
+  def multMatrizRecPar(m1:Matriz , m2: Matriz ): Matriz = {
+
+    def auxSumaVectorMatriz(matriz: Matriz, posicion: Int, acumuladorSuma : Vector[Vector[Int]]): Vector[Int]={
+      if (posicion == matriz.length) {acumuladorSuma(0)}
+      else{
+        val suma = sumMatriz(subMatriz(matriz,(posicion),0,matriz.length),acumuladorSuma.appended(Vector(0)))
+        auxSumaVectorMatriz(matriz,posicion+1,suma)
+      }
+    }
+
+    def modificarVector(vector:Vector[Int], posicion:Int,tamanio:Int, acumulador:Vector[Vector[Int]]) :Matriz={
+      if (posicion == tamanio) {acumulador}
+      else{
+        val nuevoVector = acumulador:+Vector(vector(posicion))
+        modificarVector(vector,posicion+1,tamanio,nuevoVector)
+      }
+    }
+
+    def auxMultMatrizRec (m1:Matriz , m2: Matriz,auxMatriz: Matriz, tamanio : Int,posicionQuieta:Int,posicionCambiante:Int,auxVector: Vector[Int] ) : Matriz ={
+     if (auxMatriz.length == m1.length ) {auxMatriz}
+     else{
+        val vectorSuma = Vector.tabulate(tamanio) { i =>
+          val parResul = parallel(
+            subMatriz(m1, posicionQuieta, i, m1.length)(0),
+            transpuesta(subMatriz(m2, i, posicionCambiante, m2.length))(0)
+          )
+          prodPunto(parResul._1, parResul._2)
+        }
+      val nuevoVector= modificarVector(vectorSuma,0,vectorSuma.length,Vector())
+      val aux2 = auxVector ++ auxSumaVectorMatriz(nuevoVector,0,Vector())
+      
+      if(aux2.length == tamanio){
+        val nuevaMatriz = auxMatriz:+aux2
+        auxMultMatrizRec(m1,m2,nuevaMatriz,tamanio,posicionQuieta+1,0,Vector())
+
+      }else{
+        if (posicionCambiante == tamanio-1) {auxMultMatrizRec(m1,m2,auxMatriz,tamanio,posicionQuieta+1,0,aux2)}
+      else{ auxMultMatrizRec(m1,m2,auxMatriz,tamanio,posicionQuieta,posicionCambiante+1,aux2)}
+      
+      } 
+    }
+    }
+    val matriz: Matriz= Vector()
+
+    auxMultMatrizRec(m1,m2,matriz,m1.length,0,0,Vector())
+  }
+
+
+  def strassen(A: Matriz, B: Matriz): Matriz = {
+    if (A.length == 1) {
+      Vector(Vector(A(0)(0) * B(0)(0)))
+    } else {
+
+      val newSize = A.length
+
+      //Divide las matrices en 4 bloques
+      val mitad = newSize / 2
+      val A11 = subMatriz2(A,0, mitad,0, mitad)
+      val A12 = subMatriz2(A,0, mitad,mitad, newSize)      
+      val A21 = subMatriz2(A,mitad, newSize,0, mitad)      
+      val A22 = subMatriz2(A,mitad, newSize,mitad, newSize)
+
+      val B11 = subMatriz2(B,0, mitad,0, mitad)
+      val B12 = subMatriz2(B,0, mitad,mitad, newSize)
+      val B21 = subMatriz2(B,mitad, newSize,0, mitad)
+      val B22 = subMatriz2(B,mitad, newSize,mitad, newSize)
+
+
+      val P1 = strassen(A11, restaMatriz(B12, B22))
+      val P2 = strassen(sumMatriz(A11, A12), B22)
+      val P3 = strassen(sumMatriz(A21,A22), B11)
+      val P4 = strassen(A22, restaMatriz(B21, B11))
+      val P5 = strassen(sumMatriz(A11, A22), sumMatriz(B11,B22))
+      val P6 = strassen(restaMatriz(A12, A22), sumMatriz(B21, B22))
+      val P7 = strassen(restaMatriz(A11, A21), sumMatriz(B11, B12))
+
+
+      val C11 = sumMatriz(sumMatriz(P5, P4), restaMatriz(P6, P2))
+      val C12 = sumMatriz(P1, P2)
+      val C21 = sumMatriz(P3, P4)
+      val C22 = restaMatriz(sumMatriz(P5, P1), sumMatriz(P7, P3))
+
+    val result: Matriz = Vector.tabulate(newSize) { i =>
+      if (i < mitad) {
+        Vector.concat(C11(i), C12(i))
+      } else {
+        Vector.concat(C21(i - mitad), C22(i - mitad))
+      }
+    }
+      result
+    }
+  }
+
+
+def strassenParallel(A: Matriz, B: Matriz): Matriz = {
+    if (A.length == 1) {
+      Vector(Vector(A(0)(0) * B(0)(0)))
+    } else {
+
+      val newSize = A.length
+
+      //Divide las matrices en 4 bloques
+      val mitad = newSize / 2
+      val A11 = subMatriz2(A,0, mitad,0, mitad)
+      val A12 = subMatriz2(A,0, mitad,mitad, newSize)      
+      val A21 = subMatriz2(A,mitad, newSize,0, mitad)      
+      val A22 = subMatriz2(A,mitad, newSize,mitad, newSize)
+
+      val B11 = subMatriz2(B,0, mitad,0, mitad)
+      val B12 = subMatriz2(B,0, mitad,mitad, newSize)
+      val B21 = subMatriz2(B,mitad, newSize,0, mitad)
+      val B22 = subMatriz2(B,mitad, newSize,mitad, newSize)
+
+
+      val P1 = task(strassenParallel(A11, restaMatriz(B12, B22)))
+      val P2 = task(strassenParallel(sumMatriz(A11, A12), B22))
+      val P3 = task(strassenParallel(sumMatriz(A21,A22), B11))
+      val P4 = task(strassenParallel(A22, restaMatriz(B21, B11)))
+      val P5 = task(strassenParallel(sumMatriz(A11, A22), sumMatriz(B11,B22)))
+      val P6 = task(strassenParallel(restaMatriz(A12, A22), sumMatriz(B21, B22)))
+      val P7 = task(strassenParallel(restaMatriz(A11, A21), sumMatriz(B11, B12)))
+
+      
+      val C11 = sumMatriz(sumMatriz(P5.join(), P4.join()), restaMatriz(P6.join(), P2.join()))
+      val C12 = sumMatriz(P1.join(), P2.join())
+      val C21 = sumMatriz(P3.join(), P4.join())
+      val C22 = restaMatriz(sumMatriz(P5.join(), P1.join()), sumMatriz(P7.join(), P3.join()))
+
+    val result: Matriz = Vector.tabulate(newSize) { i =>
+      if (i < mitad) {
+        Vector.concat(C11(i), C12(i))
+      } else {
+        Vector.concat(C21(i - mitad), C22(i - mitad))
+      }
+    }
+      result
+    }
+  }
+
 }
